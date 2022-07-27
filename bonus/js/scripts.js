69351af09639398cc80de389729908f44b24ecf3
spellcry <sangiothebest@hotmail.com>
2022-07-27 12:16:19 +0200

    add dots

--- bonus/js/scripts.js
+++ bonus/js/scripts.js
@@ -8,8 +8,14 @@ const slides = [
 
 const firstIndex = 0;
 
-let firstSlide, lastSlide;
+let firstSlide, lastSlide, firstDot, lastDot;
 
+// prendo elemento 'dots'
+const dotsEl = document.querySelector('.dots');
+dotsEl.innerHTML = '';
+console.log(dotsEl);
+
+// prendo elemento 'slides-wrapper'
 const slidesWrapperEl = document.querySelector('.slides-wrapper');
 slidesWrapperEl.innerHTML = '';
 
@@ -19,9 +25,6 @@ for ( let i = 0; i < slides.length; i++ ) {
     // creando li
     const liEl = document.createElement('li');
     liEl.className = 'slide';
-    if ( i === firstIndex ) {
-        liEl.classList.add('active');
-    }
 
     // creando img
     const imgEl = document.createElement('img');
@@ -32,16 +35,33 @@ for ( let i = 0; i < slides.length; i++ ) {
 
     // inseriamo li dentro la lista 'slidesWrapperEl'
     slidesWrapperEl.append(liEl);
+    
+    // creo dot
+    const dotEl = document.createElement('div');
+    dotEl.className = 'dot';
+    dotEl.classList.add(`dot${i+1}`);
 
-    // salvo primo li
+    // aggiungo la classe active al dot e alla slide desiderata
+    if ( i === firstIndex ) {
+        liEl.classList.add('active');
+        dotEl.classList.add('active');
+    }
+
+    // salvo primo li e dot
     if ( i === 0 ) {
         firstSlide = liEl;
+        firstDot = dotEl;
     }
 
-    // salvo ultimo li
+    // salvo ultimo li e dot
     if ( i === slides.length - 1 ) {
         lastSlide = liEl;
+        lastDot = dotEl;
     }
+
+    // metto dot dentro a dots
+    dotsEl.append(dotEl);
+
 }
 
 const nextArrowEl = document.querySelector('.arrow-next');
@@ -49,11 +69,20 @@ const nextArrowEl = document.querySelector('.arrow-next');
 nextArrowEl.addEventListener('click', function() {
     const activeEl = document.querySelector('.slide.active');
     activeEl.classList.remove('active');
+    const activeDotEl = dotsEl.querySelector('.active');
+    activeDotEl.classList.remove('active');
     if ( activeEl.nextElementSibling != null ) { // se c'è un elemento successivo lo visualizzo
+        // slide
         const nextLiEl = activeEl.nextElementSibling;
         nextLiEl.classList.add('active');
+        // dot
+        const nextDotEl = activeDotEl.nextElementSibling;
+        nextDotEl.classList.add('active');
     } else { // altrimenti visualizzo il primo
+        // slide
         firstSlide.classList.add('active');
+        //dot
+        firstDot.classList.add('active');
     }
 });
 
@@ -62,10 +91,41 @@ const prevArrowEl = document.querySelector('.arrow-prev');
 prevArrowEl.addEventListener('click', function() {
     const activeEl = document.querySelector('.slide.active');
     activeEl.classList.remove('active');
+    const activeDotEl = dotsEl.querySelector('.active');
+    activeDotEl.classList.remove('active');
     if ( activeEl.previousElementSibling != null ) { // se c'è un elemento precedente lo visualizzo
+        // slide
         const prevLiEl = activeEl.previousElementSibling;
         prevLiEl.classList.add('active');
+        // dot
+        const prevDotEl = activeDotEl.previousElementSibling;
+        prevDotEl.classList.add('active');
     } else { // altrimenti visualizzo l'ultimo
+        //slide
         lastSlide.classList.add('active');
+        //dot
+        lastDot.classList.add('active');
     }
+});
+
+dotsEl.addEventListener('click', function(){
+    let dotEl = dotsEl.querySelector(':hover');
+    let index = 0;
+    // trovo l'index dell'elemento
+    while( (dotEl = dotEl.previousSibling) != null ) {
+        index++;
+    }
+    // prendo la slide corrente e la prossima da visualizzare
+    const activeSlideEl = document.querySelector('.active');
+    const nextActiveSlideEl = slidesWrapperEl.childNodes[index];
+    
+    activeSlideEl.classList.remove('active');
+    nextActiveSlideEl.classList.add('active');
+
+    // prendo il dot corrente e il prossimo attivo
+    const activeDotEl = dotsEl.querySelector('.active');
+    const nextActiveDotEl = dotsEl.childNodes[index];
+
+    activeDotEl.classList.remove('active');
+    nextActiveDotEl.classList.add('active');
 });
